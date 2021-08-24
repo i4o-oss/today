6a6a03c5b3cca93b10e2b9b86e9e1bacf3914fd3
Ilango <rajagopalilango@gmail.com>
2021-08-24 07:57:25 +0530

    Check for session on homepage and render different things

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,25 @@
-// import {} from '@chakra-ui/react';
-
+import { useEffect, useState } from 'react';
+import { supabase } from '../lib/supabaseClient';
 import Container from '../components/common/Container';
 import Header from '../components/common/Header';
+import LandingPage from '../components/common/LandingPage';
+import Feed from '../components/app/Feed';
 
 function Index() {
+	const [session, setSession] = useState(null);
+
+	useEffect(() => {
+		setSession(supabase.auth.session());
+
+		supabase.auth.onAuthStateChange((event, session) => {
+			setSession(session);
+		});
+	}, []);
+
 	return (
 		<Container height='100vh'>
 			<Header />
+			{session ? <Feed /> : <LandingPage />}
 		</Container>
 	);
 }
