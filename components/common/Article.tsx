08ab4e09aa3e14fdde956a0982dd8de229673046
Ilango <rajagopalilango@gmail.com>
2021-08-31 03:46:50 +0530

    Extract article component from rss block for reuse

---
+++ components/common/Article.tsx
@@ -0,0 +1,34 @@
+import {Flex, Heading, Link, Text, useColorModeValue} from "@chakra-ui/react";
+import truncate from 'lodash.truncate';
+import { format } from "date-fns";
+
+interface Article {
+    date: string;
+    link: string;
+    summary: string;
+    title: string;
+}
+
+export default function Article(props: Article) {
+    const { date, link, summary, title } = props;
+
+    return (
+        <Link isExternal={true} href={link} _hover={{ textDecoration: 'none' }}>
+            <Flex
+                flexDirection='column'
+                p={6}
+                mb={4}
+                borderWidth={1}
+                borderStyle='solid'
+                borderColor={useColorModeValue('gray.600', 'gray.400')}
+                borderRadius='lg'
+            >
+                <Heading as='h4' fontSize='xl' mb={2}>
+                    {title}
+                </Heading>
+                <Text mb={2}>{truncate(summary, { length: 320 })}</Text>
+                <Text>{format(new Date(date), 'MMM dd')}</Text>
+            </Flex>
+        </Link>
+    );
+}
