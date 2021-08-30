b25205409b815605dc03c3e91f9fdab5ea36cff3
Ilango <rajagopalilango@gmail.com>
2021-08-31 03:48:21 +0530

    Remove drag and drop code

--- components/app/Feed.tsx
+++ components/app/Feed.tsx
@@ -1,10 +1,10 @@
 import { Flex, Grid, Heading } from '@chakra-ui/react';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
-import RSS from '../blocks/RSS';
-import { reorder } from '../../lib/utils';
-import today from '../../today.config';
 import TodayDate from '../blocks/TodayDate';
 import Weather from '../blocks/Weather';
+import RSS from '../blocks/RSS';
+import HackerNews from "../blocks/HackerNews";
+import today from '../../today.config';
 
 interface Feed {
 	blocks: string[];
@@ -12,31 +12,13 @@ interface Feed {
 }
 
 function Feed(props: Feed): JSX.Element {
-	const { blocks, updateBlocks } = props;
-
-	const onDragEnd = (result) => {
-		if (!result.destination) {
-			return;
-		}
-
-		if (result.destination.index === result.source.index) {
-			return;
-		}
-
-		const updatedFeeds = reorder(
-			blocks,
-			result.source.index,
-			result.destination.index
-		);
-
-		updateBlocks(updatedFeeds);
-	};
+	const { blocks } = props;
 
 	return (
 		<Flex
 			w='full'
 			maxW='container.lg'
-			py={32}
+			py={16}
 			flexDirection='column'
 			alignItems='start'
 			justifyContent='start'
@@ -45,52 +27,47 @@ function Feed(props: Feed): JSX.Element {
 			<Heading as='h1' fontSize='8xl' fontWeight='thin' mb={16}>
 				Today
 			</Heading>
-			<DragDropContext onDragEnd={onDragEnd}>
-				<Droppable droppableId='today'>
-					{(provided) => (
-						<Grid
-							gap={4}
-							maxW='container.lg'
-							templateColumns='repeat(2, 1fr)'
-							w='full'
-							ref={provided.innerRef}
-							{...provided.droppableProps}
-						>
-							{blocks.map((block, index) => {
-								switch (block) {
-									case 'date': {
-										return <TodayDate key={index} />;
-									}
+			<Grid
+				gap={4}
+				maxW='container.lg'
+				templateColumns='repeat(2, 1fr)'
+				w='full'
+			>
+				{blocks.map((block, index) => {
+					switch (block) {
+						case 'date': {
+							return <TodayDate key={index} />;
+						}
 
-									case 'weather': {
-										return <Weather key={index} />;
-									}
+						case 'weather': {
+							return <Weather key={index} />;
+						}
 
-									case 'rss': {
-										const { rss } = today?.blocks;
-										const { feeds, name, size } = rss;
-										return (
-											<RSS
-												block={block}
-												feeds={feeds}
-												key={index}
-												index={index}
-												name={name}
-												size={size}
-											/>
-										);
-									}
+						case 'rss': {
+							const { rss } = today?.blocks;
+							const { feeds, name, size } = rss;
+							return (
+								<RSS
+									block={block}
+									feeds={feeds}
+									key={index}
+									index={index}
+									name={name}
+									size={size}
+								/>
+							);
+						}
 
-									case 'hackernews': {
-										break;
-									}
-								}
-							})}
-							{provided.placeholder}
-						</Grid>
-					)}
-				</Droppable>
-			</DragDropContext>
+						case 'hackernews': {
+							const { hackernews } = today?.blocks;
+							const { name, size, url } = hackernews;
+							return (
+								<HackerNews key={index} name={name} size={size} url={url} />
+							);
+						}
+					}
+				})}
+			</Grid>
 		</Flex>
 	);
 }
