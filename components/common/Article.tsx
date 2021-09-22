ab1fd12d5c9d2053c64199405533daefa7df48aa
Ilango <rajagopalilango@gmail.com>
2021-09-22 10:13:27 +0530

    Update article component styles

--- components/common/Article.tsx
+++ components/common/Article.tsx
@@ -23,37 +23,33 @@ export default function Article(props: Article) {
 	const { date, link, image, siteTitle, summary, title } = props;
 
 	return (
-		<HStack d='flex' alignItems='start' py={4} mb={6} spacing={4} w='full'>
-			<Flex flexDirection='column'>
-				<Link
-					isExternal={true}
-					href={link}
-					_hover={{ textDecoration: 'none' }}
-				>
-					<Heading as='h4' fontSize='2xl' mb={2}>
-						{title}
-					</Heading>
-				</Link>
-				<Text fontSize='xl' mb={2}>
-					{truncate(summary, { length: 140 })}
-				</Text>
-				{siteTitle ? (
-					<Flex alignItems='center'>
-						{image ? (
-							<Image src={image} w='24px' h='24px' mr={2} />
-						) : null}
-						<Text fontSize='lg'>
-							{siteTitle}
-							{'  '}&bull;{'  '}
-							{format(new Date(date), 'MMM dd')}
-						</Text>
-					</Flex>
-				) : (
+		<Flex flexDirection='column' py={6}>
+			<Link
+				isExternal={true}
+				href={link}
+				_hover={{ textDecoration: 'none' }}
+			>
+				<Heading as='h4' fontSize='2xl' mb={2}>
+					{title}
+				</Heading>
+			</Link>
+			<Text fontSize='xl' mb={2}>
+				{truncate(summary, { length: 140 })}
+			</Text>
+			{siteTitle ? (
+				<Flex alignItems='center'>
+					{image ? (
+						<Image src={image} w='24px' h='24px' mr={2} />
+					) : null}
 					<Text fontSize='lg'>
+						{siteTitle}
+						{'  '}&bull;{'  '}
 						{format(new Date(date), 'MMM dd')}
 					</Text>
-				)}
-			</Flex>
-		</HStack>
+				</Flex>
+			) : (
+				<Text fontSize='lg'>{format(new Date(date), 'MMM dd')}</Text>
+			)}
+		</Flex>
 	);
 }
