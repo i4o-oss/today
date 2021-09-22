bf3a0c61c11249f71ba6b7b9786aec585fbb4f1c
Ilango <rajagopalilango@gmail.com>
2021-09-22 18:54:17 +0530

    Get weather from openweathermap api

--- components/configuration/Weather.tsx
+++ components/configuration/Weather.tsx
@@ -1,24 +1,107 @@
-import { useState } from 'react';
-import { Button, Flex, Grid, Input, Text, VStack } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import {
+	Button,
+	Flex,
+	Grid,
+	HStack,
+	IconButton,
+	Input,
+	InputGroup,
+	Text,
+	VStack,
+} from '@chakra-ui/react';
+import { BiCurrentLocation } from 'react-icons/bi';
 import { ConfigureBlockProps } from '../../lib/types';
 
 function Weather(props: ConfigureBlockProps): JSX.Element {
 	const [blockTitle, setBlockTitle] = useState('Weather');
+	const [city, setCity] = useState('');
+	const [coordinates, setCoordinates] = useState(undefined);
+	// const [loading, setLoading] = useState(false);
+	const [weather, setWeather] = useState(undefined);
+
+	async function fetchWeather() {
+		// setLoading(true);
+		if (coordinates) {
+			const response = await fetch(
+				`/api/weather?lat=${coordinates?.latitude}&lon=${coordinates?.longitude}`
+			);
+			const { weather } = await response.json();
+			setWeather(weather);
+		} else if (city) {
+			const response = await fetch(
+				`/api/weather?city=${city.toLowerCase()}`
+			);
+			const { weather } = await response.json();
+			setWeather(weather);
+		}
+		// setLoading(false);
+	}
+
+	useEffect(() => {
+		fetchWeather().then(() => console.log('Fetched Weather! ✨'));
+	}, [coordinates]);
 
 	function saveWeatherBlock() {
-		const data = {};
+		const data = {
+			coordinates,
+			title: blockTitle,
+		};
 		props.saveBlock(data);
+	}
+
+	function requestLocation() {
+		navigator.geolocation.getCurrentPosition((position) =>
+			setCoordinates({
+				latitude: position.coords.latitude,
+				longitude: position.coords.longitude,
+			})
+		);
+	}
+
+	function cityKeyPressHandler(e) {
+		if (e.keyCode === 13) {
+			fetchWeather().then(() => console.log('Fetched Weather! ✨'));
+		}
 	}
 
 	return (
 		<VStack w='full' spacing={6}>
 			<Grid gap={4} templateColumns='repeat(2, 1fr)' w='full'>
-				<Flex w='full' alignItems='center' justifyContent='start'>
-					<Text fontSize='lg'>Location</Text>
+				<Flex
+					w='full'
+					alignItems='center'
+					justifyContent='space-between'
+				>
+					<Text fontSize='lg'>City/Location</Text>
 				</Flex>
-				<Flex w='full' alignItems='center' justifyContent='end'>
-					<Button>Detect Location</Button>
-				</Flex>
+				<HStack
+					w='full'
+					alignItems='center'
+					justifyContent='space-between'
+					spacing={2}
+				>
+					<InputGroup>
+						<Input
+							w='auto'
+							h={12}
+							d='flex'
+							flex='1'
+							onChange={(e) => setCity(e.target.value)}
+							onKeyDown={cityKeyPressHandler}
+							value={city}
+						/>
+					</InputGroup>
+					<IconButton
+						w={12}
+						h={12}
+						p={0}
+						fontSize='xl'
+						aria-label='Detect Location'
+						icon={<BiCurrentLocation />}
+						onClick={requestLocation}
+					/>
+				</HStack>
 				<Flex
 					w='full'
 					alignItems='center'
@@ -39,6 +122,7 @@ function Weather(props: ConfigureBlockProps): JSX.Element {
 				</Flex>
 			</Grid>
 			<Flex w='full' alignItems='center' justifyContent='end'>
+				{weather ? <Text mr={4}>Location Saved</Text> : null}
 				<Button colorScheme='brand' onClick={saveWeatherBlock}>
 					Save
 				</Button>
---
+++ pages/api/weather.ts
@@ -0,0 +1,31 @@
+import nc from 'next-connect';
+
+export async function currentWeather(req, res) {
+	const { lat, lon, city } = req.query;
+
+	if (lat && lon) {
+		const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${process.env.OWM_API_KEY}`;
+		const response = await fetch(url, {
+			method: 'GET',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+		});
+		const weather = await response.json();
+		res.status(200).json({ weather });
+	} else if (city) {
+		const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&exclude=minutely,hourly,alerts&appid=${process.env.OWM_API_KEY}`;
+		const response = await fetch(url, {
+			method: 'GET',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+		});
+		const weather = await response.json();
+		res.status(200).json({ weather });
+	}
+}
+
+export default nc({ attachParams: true }).get(currentWeather);
