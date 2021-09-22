ac8593a4849a69f1a151818e649dedafd903fb6d
Ilango <rajagopalilango@gmail.com>
2021-09-22 19:52:19 +0530

    Update owm weather endpoint

--- pages/api/weather.ts
+++ pages/api/weather.ts
@@ -4,7 +4,7 @@ export async function currentWeather(req, res) {
 	const { lat, lon, city } = req.query;
 
 	if (lat && lon) {
-		const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${process.env.OWM_API_KEY}`;
+		const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.OWM_API_KEY}`;
 		const response = await fetch(url, {
 			method: 'GET',
 			headers: {
